520d88eb72b947ea915d36a018b41b071b82f2fd
mdeveloper66 <m.developer66@gmail.com>
2021-01-27 16:10:11 +0330

    Add static files and make a static index file

---
+++ catdex/src/main.rs
@@ -0,0 +1,19 @@
+use actix_files::{Files, NamedFile};
+use actix_web::{web, App, HttpServer, Result};
+
+async fn index() -> Result<NamedFile> {
+    Ok(NamedFile::open("./static/index.html")?)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Listening to port 8080");
+    HttpServer::new(|| {
+        App::new()
+            .service(Files::new("/static", "static").show_files_listing())
+            .route("/", web::get().to(index))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
