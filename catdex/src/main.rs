0871446162e21d14e0bb50353ec86bc1c645981c
mdeveloper66 <m.developer66@gmail.com>
2021-01-29 11:20:57 +0330

    Add new cat with form data

--- catdex/src/main.rs
+++ catdex/src/main.rs
@@ -1,16 +1,19 @@
-use catdex::models::*;
 use actix_files::Files;
-use actix_web::{web, App, Error, HttpResponse, HttpServer};
+use actix_web::{web, App, Error, HttpResponse, HttpServer,http};
+use awmp::Parts;
+use catdex::models::*;
+use catdex::schema::cats::dsl::cats;
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use diesel::r2d2::{self, ConnectionManager};
-use handlebars::{Handlebars};
-use serde::{ Serialize};
-use catdex::schema::cats::dsl::cats;
+use handlebars::Handlebars;
+use serde::Serialize;
+use std::collections::HashMap;
+
 #[derive(Serialize)]
 struct IndexTemplateData {
     project_name: String,
-    cats: Vec<Cat>
+    cats: Vec<Cat>,
 }
 
 async fn index(
@@ -21,7 +24,7 @@ async fn index(
     let cats_data = web::block(move || cats.limit(100).load::<Cat>(&connection))
         .await
         .map_err(|_| HttpResponse::InternalServerError().finish())?;
-    let data = IndexTemplateData{
+    let data = IndexTemplateData {
         project_name: "Catdex".to_string(),
         cats: cats_data,
     };
@@ -30,6 +33,38 @@ async fn index(
 }
 
 type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
+async fn add(hb: web::Data<Handlebars<'_>>) -> Result<HttpResponse, Error> {
+    let body = hb.render("add", &{}).unwrap();
+    Ok(HttpResponse::Ok().body(body))
+}
+
+async fn add_cat_form(pool: web::Data<DbPool>, mut parts: Parts) -> Result<HttpResponse, Error> {
+    let connection = pool.get().expect("Can't get db connection from pool");
+    let file_path = parts
+        .files
+        .take("image")        
+        .pop()
+        .and_then(|f| f.persist_in("/static/images").ok())
+        .map(|f| format!("{}", f.display()))
+        .unwrap_or_default();
+
+    let text_fields: HashMap<_, _> = parts.texts.as_pairs().into_iter().collect();
+    let new_cat = NewCat {
+        name: text_fields.get("name").unwrap().to_string(),
+        image_path: file_path.to_string(),
+    };
+
+    web::block(move || {
+        diesel::insert_into(cats)
+            .values(&new_cat)
+            .execute(&connection)
+    })
+    .await
+    .map_err(|_| HttpResponse::InternalServerError().finish())?;
+
+    Ok(HttpResponse::SeeOther().header(http::header::LOCATION, "/")
+    .finish())
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -53,6 +88,8 @@ async fn main() -> std::io::Result<()> {
             .data(pool.clone())
             .service(Files::new("/static", "static").show_files_listing())
             .route("/", web::get().to(index))
+            .route("/add", web::get().to(add))
+            .route("/add_cat_form", web::post().to(add_cat_form))
     })
     .bind("127.0.0.1:8080")?
     .run()
